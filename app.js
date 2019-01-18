eb3b3e4a18b7709c850057323ea8c5948bc6258a
Taylor Conners <tconners@uncc.edu>
2019-01-18 12:28:14 -0500

    Switching from PUG to EJS

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+  // res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index', { list: ['a', 'b'] });
 });
 
-app.listen(3000, () => {
-    console.log(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening at port ${chalk.green(port)}`);
 });
